a3815086e2d45051b13e2c4b97cf1b801e11aa64
tatl9r <e.shuvaeva@outlook.com>
2022-06-02 22:52:57 +0300

    Add classes

--- Post.java
+++ Post.java
@@ -4,5 +4,6 @@ public class Post {
     public String patronymic;
     public String phone;
     public String surname;
-    public Boolean subscription
+    public String subscription;
+    public FormDate birthday
 }
